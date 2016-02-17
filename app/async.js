c81662e0770a77ab2acc98d6e6e65460c92f1b6d
mahenikhil <mahenikhil@gmail.com>
2016-02-17 12:56:09 +0530

    Added the solution to fix the test cases

--- app/async.js
+++ app/async.js
@@ -10,6 +10,22 @@ exports.asyncAnswers = {
   },
 
   manipulateRemoteData : function(url) {
-
+	var xhttp = new XMLHttpRequest();
+	
+	
+	return {
+		then :function(fn){
+			xhttp.onreadystatechange = function() {
+			if (xhttp.readyState == 4 && xhttp.status == 200) {
+				var obj=JSON.parse(xhttp.response);
+			    fn(obj.people.map(function(person){
+					return person.name;
+				}).sort());
+			}
+		  };
+			xhttp.open("GET", url, true);
+			xhttp.send();
+		}
+	}
   }
 };
