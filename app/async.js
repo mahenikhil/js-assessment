a94eacee18b75db393277a2df588652bad153d97
mahenikhil <mahenikhil@gmail.com>
2016-02-17 12:31:22 +0530

    Added the solution to fix the test cases

--- app/async.js
+++ app/async.js
@@ -2,7 +2,11 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+	return {
+		then:function(fn){
+			fn(value);
+		}
+	}
   },
 
   manipulateRemoteData : function(url) {
